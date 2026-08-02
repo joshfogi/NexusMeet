087b9736089aa1b5aec1f0e1e6177738859d01a0
joshfogi <fuckoff963@gmail.com>
2026-08-02 22:42:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const io = new Server(server, { cors: { origin: '*' } });
 const peerServer = ExpressPeerServer(server, { debug: true, path: '/' });
 
 app.use('/peerjs', peerServer);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/:room', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/room', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
@@ -38,3 +38,4 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
